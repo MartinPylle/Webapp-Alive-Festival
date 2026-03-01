8e6877c9723ed000c196b5a9c37e8d028f08b2ad
mels-dev <10700529@ucn.dk>
2026-03-01 21:31:28 +0100

    Mad sider
    Undersiderne til madboderne er sat op, stylet og linket til for hvert card. Drink boderne vil være lige til når jeg får lavet alle siderne til det.
    
    Navigation ser jeg på et andet tidspunkt. Ved ikke hvor lang tid det kommer til at ta
    
    VIGTIG NOTE:
    
    Af en eller anden grund kan man ikek trykke på cardet 'bryggen' selvom den er linket korrekt. Copilot kunne ikke hjælpe mig med at løse det, men alle andre cards linker til deres sider.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,37 @@
+
+
+
+
+
+
+
 // Melanie's kode her
 
 let foodData = [
 
-    // Bryggen
-    {picture: "assets/img/bryggen.webp", title: "Bryggen"},
+    // Bryggen 
+    {picture: "assets/img/bryggen.webp", title: "Bryggen", link: "brygge.html"},
 
-    // Den grønne fe
- {picture: "assets/img/mad_groen_fe.webp", title: "Den Grønne Fe"},
+    // Den grønne fe 
+    {picture: "assets/img/mad_groen_fe.webp", title: "Den Grønne Fe", link: "den_groenne_fe.html"},
 
     // Gadens Gastronomer
- {picture: "assets/img/gadens_gastronomer.webp", title: "Gadens Gastronomer"},
+    {picture: "assets/img/gadens_gastronomer.webp", title: "Gadens Gastronomer", link: "gadens_gastronomer.html"},
 
     // Mikuna
- {picture: "assets/img/mikuna.webp", title: "Mikuna"},
+    {picture: "assets/img/mikuna.webp", title: "Mikuna", link: "mikuna.html"},
 
     // Salke
- {picture: "assets/img/salke.webp", title: "Salke"},
+    {picture: "assets/img/salke.webp", title: "Salke", link: "salke.html"},
 
     // Streetbites Thai
- {picture: "assets/img/streetbites_thai.webp", title: "StreetBites Thai"},
+    {picture: "assets/img/streetbites_thai.webp", title: "StreetBites Thai", link: "street_bites.html"},
 
     // Thypisk pizza
- {picture: "assets/img/thypisk_pizza.webp", title: "Thypisk Pizza"},
+    {picture: "assets/img/thypisk_pizza.webp", title: "Thypisk Pizza", link: "thypisk_pizza.html"},
 
     // The taco truck
- {picture: "assets/img/taco.webp", title: "The Taco Truck"},
+    {picture: "assets/img/taco.webp", title: "The Taco Truck", link: "taco.html"},
 
 ];
 
@@ -55,23 +62,28 @@ let cards = document.querySelector(".madcards")
 
 // Alle individuelle madcards kommer gennem her
     foodData.forEach(function(card) {
-        
+
+      // Her opretter jeg et link og siger at for hver card skal den linke til den sti jeg har lavet oppe i arrays
+        let link = document.createElement("a");
+        link.href = card.link;                 
+
         // Her opretter jeg en article
-        let madArticle = document.createElement("article")
+        let madArticle = document.createElement("article");
 
         // Giver den klassen madcard
-        madArticle.classList.add("madcard")
+        madArticle.classList.add("madcard");
 
-        // Her får jeg den til at ændre baggrundsbilledet i mine articles for hvert loop
-        madArticle.style.backgroundImage = `url(${card.picture})`
+        // Baggrundsbilledet for hvert card
+        madArticle.style.backgroundImage = `url(${card.picture})`;
 
-        // Her laver jeg titlen til hvert card og gør sådan de skifter for hvert 
+        // Titel
         let madTitle = document.createElement("h3");
-
         madTitle.textContent = card.title;
 
-        madArticle.appendChild(madTitle)
-        cards.appendChild(madArticle)
+      //   Her får jeg mad cards ud i DOM
+        madArticle.appendChild(madTitle);
+        link.appendChild(madArticle);         
+        cards.appendChild(link);              
     });
 
     // Alle individuelle drikkecards
@@ -97,4 +109,7 @@ let cards2 = document.querySelector(".drikkecards")
     });
 
 
-    
+
+
+
+
