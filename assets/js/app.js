2b807a29cb141df81d0049bc2d24ffcb63c3dc26
mels-dev <10700529@ucn.dk>
2026-03-06 14:20:14 +0100

    Alt mad og drikke færdig

---
+++ assets/js/app.js
@@ -0,0 +1,119 @@
+<<<<<<< Updated upstream
+=======
+
+
+
+
+
+
+
+// Melanie's kode her
+
+let foodData = [
+
+    // Bryggen 
+    {picture: "assets/img/bryggen.webp", title: "Bryggen", link: "brygge.html"},
+
+    // Den grønne fe 
+    {picture: "assets/img/mad_groen_fe.webp", title: "Den Grønne Fe", link: "den_groenne_fe.html"},
+
+    // Gadens Gastronomer
+    {picture: "assets/img/gadens_gastronomer.webp", title: "Gadens Gastronomer", link: "gadens_gastronomer.html"},
+
+    // Mikuna
+    {picture: "assets/img/mikuna.webp", title: "Mikuna", link: "mikuna.html"},
+
+    // Salke
+    {picture: "assets/img/salke.webp", title: "Salke", link: "salke.html"},
+
+    // Streetbites Thai
+    {picture: "assets/img/streetbites_thai.webp", title: "StreetBites Thai", link: "street_bites.html"},
+
+    // Thypisk pizza
+    {picture: "assets/img/thypisk_pizza.webp", title: "Thypisk Pizza", link: "thypisk_pizza.html"},
+
+    // The taco truck
+    {picture: "assets/img/taco.webp", title: "The Taco Truck", link: "taco.html"},
+
+];
+
+let drinkData = [
+    // Bryghusbar
+    {picture: "assets/img/bryghus_beer.webp", title: "BryghusBar", link: "bryghus_beer.html"},
+
+    // Bærbar
+    {picture: "assets/img/baerbar.webp", title: "Bærbar", link: "baerbar.html"},
+
+     // Cocktailbar
+    {picture: "assets/img/cocktailbar.webp", title: "Cocktailbar", link: "cocktailbar.html"},
+
+     // Havebar
+    {picture: "assets/img/havebar.webp", title: "Havebar", link: "have.html"},
+
+     // Skovbar
+    {picture: "assets/img/skovbar.webp", title: "Skovbar", link: "skov.html"},
+
+    // Vadebar
+    {picture: "assets/img/vadebar.webp", title: "Vadebar", link: "vade.html"},
+
+     // Vin og whiskey lounge
+    {picture: "assets/img/vin_whiskey_lounge.webp", title: "Vin- og Whiskey loungen", link: "vin.html"},
+]
+
+let cards = document.querySelector(".madcards")
+
+// Alle individuelle madcards kommer gennem her
+    foodData.forEach(function(card) {
+
+      // Her opretter jeg et link og siger at for hver card skal den linke til den sti jeg har lavet oppe i arrays
+        let link = document.createElement("a");
+        link.href = card.link;                 
+
+        // Her opretter jeg en article
+        let madArticle = document.createElement("article");
+
+        // Giver den klassen madcard
+        madArticle.classList.add("madcard");
+
+        // Baggrundsbilledet for hvert card
+        madArticle.style.backgroundImage = `url(${card.picture})`;
+
+        // Titel
+        let madTitle = document.createElement("h3");
+        madTitle.textContent = card.title;
+
+      //   Her får jeg mad cards ud i DOM
+        madArticle.appendChild(madTitle);
+        link.appendChild(madArticle);         
+        cards.appendChild(link);              
+    });
+
+    // Alle individuelle drikkecards
+let cards2 = document.querySelector(".drikkecards")
+
+     drinkData.forEach(function(card2) {
+
+        // Link til drinks sider
+        let link = document.createElement("a");
+        link.href = card2.link;   
+        
+        // Her opretter jeg en article
+        let drinkArticle = document.createElement("article")
+
+        // Giver den klassen madcard
+        drinkArticle.classList.add("drinkcard")
+
+        // Her får jeg den til at ændre baggrundsbilledet i mine articles for hvert loop
+        drinkArticle.style.backgroundImage = `url(${card2.picture})`
+
+        // Her laver jeg titlen til hvert card og gør sådan de skifter for hvert 
+        let drinkTitle = document.createElement("h3");
+        drinkTitle.textContent = card2.title;
+
+        // Push det ud i DOM
+
+        drinkArticle.appendChild(drinkTitle);
+        link.appendChild(drinkArticle);         
+        cards2.appendChild(link);    
+    });
+>>>>>>> Stashed changes
