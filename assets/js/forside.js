56227f1e66f2299cefab49c6269d7ac8d213da37
Martin <10461942@ucn.dk>
2026-03-08 15:08:02 +0100

    små ændringer og skrevet kommentar til JS

--- assets/js/forside.js
+++ assets/js/forside.js
@@ -3,46 +3,67 @@
 // Slideshow
 // https://www.w3schools.com/howto/howto_js_slideshow.asp
 
+// Variabel der holder styr på hvilket slide der vises
 let slideIndex = 0;
+// Variabel der gemmer timeren for automatisk slideshow
 let timer; // add this
 
+// Laver en funktion med navnet showSlides 
 function showSlides() {
+  // Henter alle elementer med klassen .mySlides
   let slides = document.querySelectorAll(".mySlides");
+  // Henter alle elementer med klassen .dot
   let dots = document.querySelectorAll(".dot");
   
+  // loop der skjuler alle slides
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
+  // Fjerner "active" klassen fra alle dots
   dots.forEach(d => d.classList.remove("active"));
 
+  // Går videre til næste slide
   slideIndex++;
+  // Hvis slideshow er nået til sidste slide, start forfra
   if (slideIndex > slides.length) { slideIndex = 1; }
+  // Hvis slideIndex er mindre end 1, vis sidste slide
   if (slideIndex < 1) {slideIndex = slides.length; }
 
+  // Viser den aktuelle slide
   slides[slideIndex - 1].style.display = "block";
+  // Marker den tilhørende dot som aktiv
   dots[slideIndex -1].classList.add("active");
-
+  // starter automatisk slideshow igen efter 10 sekunder 
   timer = setTimeout(showSlides, 10000); // store the timer
 }
 
+// Funktion til at skifte slide ved klik på pileknapper
+
 function plusSlides(n) {
-  clearTimeout(timer); // cancel auto-advance
+  // Stopper den automatiske slideshow timer
+  clearTimeout(timer);
+  // ændre slideIndex afhængigt af om man går frem eller tilbage 
   slideIndex += n - 1;
+  // kalder showSlides igen og opdatere slideshowet
   showSlides();
 }
+
+// Funktion til at vælge et bestemt slide via dots
 
 function currentSlide(n) {
+  // Stopper den automatiske slideshow timer
   clearTimeout(timer);
+  // Sætter slideIndex til det valgte slide
   slideIndex = n - 1;
+  // Opdaterer slideshowet
   showSlides();
 }
-
+// Starter slideshowet når siden loader
 showSlides();
 
 
 // Nyhedscards
-
+// Oprettet en array med objekter inden i 
 const news = [
   {
     title: {
@@ -93,12 +114,15 @@ const news = [
   },
 ];
 
-// Fang det sted, hvor article skal vises
-
+// Fang det sted, hvor article skal vises 
 const cardEL = document.querySelector(".cards")
 
 // loop gennem news og indsæt en article for hver nyhed
 // Der bruges DOM manipulation til at overskrive det eksisterende HTML 
+
+// for hver nyhed. Data fra objekterne indsættes i HTML ved hjælp af 
+// Template literals og vises derefter i websiden.
+// Back ticks bruges til template literals og gør det muligt at skrive HTML over flere linjer og indsættes med variablen ${}
 news.forEach(function(nyhed) {
    cardEL.innerHTML += `
    		<article class="card">
